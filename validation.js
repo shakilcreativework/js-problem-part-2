8bba60bc7e91566c096ed1b19235842914d5dcfe
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-06 14:21:32 +0600

    Array.isArray check and condition

--- validation.js
+++ validation.js
@@ -22,7 +22,30 @@ const full = fullName('shakil', 'ahmed');
 // console.log(full);
 
 
-function getPrice(product){
-
+function getPrice(products){
+    // console.log(typeof products);
+    if(typeof products !== 'object'){
+        return 'please provide a valid products object';
+    }
+    return products;
 }
 
+// const price = getPrice([]);
+// const price = getPrice(5);
+// const price = getPrice('shakil');
+const price = getPrice({name: 'chulkani dandi', price: 35, color: 'blue'});
+// console.log(price.color);
+
+
+function getSecond(numbers){
+    // console.log(typeof numbers);
+    if(!Array.isArray(numbers)){
+        return 'please provide an array';
+    }
+    const second = numbers[1];
+    return second;
+}
+
+// const second = getSecond([1, 4, 6, 7, 29]);
+const second = getSecond(43);
+// console.log(second);
