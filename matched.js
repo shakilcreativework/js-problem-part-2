eb0a01c2f299e34247913d3bb0fa6e2e994ac76c
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-07 19:37:41 +0600

    how to get matched products

---
+++ matched.js
@@ -0,0 +1,30 @@
+const numbers = [34, 53, 63, 74, 74, 74, 78];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const number = numbers[i];
+//     console.log(number);
+// }
+
+
+// for(const number of numbers){
+//     console.log(number);
+// }
+
+
+const products = [
+    {id: 1, name: 'walton phone', price: 19000},
+    {id: 2, name: 'iphone', price: 199000},
+    {id: 3, name: 'mac book air', price: 119000},
+    {id: 4, name: 'lenovo yoga laptop 2025', price: 19000},
+    {id: 5, name: 'Dell', price: 19000},
+    {id: 6, name: 'Nokia', price: 19000},
+];
+
+// for(const product of products){
+//     console.log(product);
+// }
+
+
+function matchedProducts(products, search){
+    
+}
