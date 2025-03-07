19754365444d00935235bda678652bd8a5277f27
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-07 22:12:40 +0600

    search and get matched your condition search result

--- matched.js
+++ matched.js
@@ -12,7 +12,7 @@ const numbers = [34, 53, 63, 74, 74, 74, 78];
 
 
 const products = [
-    {id: 1, name: 'walton phone', price: 19000},
+    {id: 1, name: 'walton phone', price: 12000},
     {id: 2, name: 'iphone', price: 199000},
     {id: 3, name: 'mac book air', price: 119000},
     {id: 4, name: 'lenovo yoga laptop 2025', price: 19000},
@@ -26,5 +26,32 @@ const products = [
 
 
 function matchedProducts(products, search){
-    
+    for(const product of products){
+        // console.log(product);
+        if(product.name === search){
+            // console.log(product);
+            return product;
+        }
+    }
 }
+
+// const result = matchedProducts(products, 'iphone');
+// console.log(result);
+// console.log(result.id);
+// console.log(result.name);
+// console.log(result.price);
+
+
+function matchedPriceRange(products, price){
+    // console.log(products, price);
+    let search = [];
+    for(const product of products){
+        if(product.price >= price){
+            search.push(product);
+        }
+    }
+    return search;
+}
+
+const result = matchedPriceRange(products, 19000);
+console.log(result);
