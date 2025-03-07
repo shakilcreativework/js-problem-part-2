65ce177a0e1d544aa567b964bfbfabd5b0137285
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-07 19:15:42 +0600

    an object write variable, object, array and in this write can be same ways

---
+++ object.js
@@ -0,0 +1,19 @@
+const student = {
+    name: 'Shakil Ahmed',
+    id: 121,
+    address: 'movie cinema',
+    isSingle: true,
+    friends: ['Apu', 'Raaz', 'Salman', 'Amir'],
+    car: {
+        brand: 'tesla',
+        price: 50000000,
+        made: 2025,
+        manufacturer: {
+            name: 'tesla',
+            ceo: 'Elon Mask',
+            country: 'USA'
+        }
+    }
+};
+
+console.log(student.friends);
